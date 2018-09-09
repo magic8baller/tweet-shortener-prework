4e96e92e3edfe4c98f33d1504ee514ef796ec22b
Leslie Turis <lturis@gmail.com>
2018-09-09 18:31:04 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,6 +29,8 @@ end
 
 
 def selective_tweet_shortener(tweet)
-  word_substituter(tweet) if tweet.size > 140
-  tweet
+  if tweet.size > 140
+      word_substituter(tweet) 
+  else tweet
+  end
 end
