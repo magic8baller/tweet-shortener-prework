f1266e3495467e60b5a95ee84177f55b4ea4ce97
Leslie Turis <lturis@gmail.com>
2018-09-09 18:40:37 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -37,7 +37,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).size > 140
-    word_substituter(tweet)[0..137] + '...'
+    word_substituter(tweet)[0..135] + '...'
   else tweet
   end
 end
